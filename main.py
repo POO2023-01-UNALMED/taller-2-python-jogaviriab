b4c2c3a549e5dac899650b19440ac4d88e6e87c0
jogaviriab <124937403+jogaviriab@users.noreply.github.com>
2023-02-20 15:17:31 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+class Asiento():
+    def __init__(self,color,precio,registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(self, color):
+        if color == "rojo":
+            self.color = color
+        elif color == "verde":
+            self.color = color
+        elif color == "amarillo":
+            self.color = color
+        elif self.color == "negro":
+            self.color == color
+        elif self.color == "blanco":
+            self.color = color
+        
+
+class Motor():
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+    
+    def asignarTipo(self, tipo):
+        if tipo == "electrico":
+            self.tipo = tipo
+        elif tipo == "gasolina":
+            self.tipo = tipo
+
+
+class Auto():
+    def __init__(self, modelo,precio,asientos,marca,motor,registro,cantidadCreados):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        self.cantidadCreados = cantidadCreados
+    
+    def cantidadAsientos(self):
+        print(len(self.asientos))
+
+        
+
+    def verificarRegistro(self):
+        if self.registro == self.motor.registro and self.registro == self.aientos.registro:
+            print("Auto original")
+        else:
+            print("Las piezas no son originales")
+
+
+        
